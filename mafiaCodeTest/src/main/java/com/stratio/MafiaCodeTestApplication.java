f875a6ba79ca7d841719423ae7d352e81c036874
ÂOlgaJuarezArranz <ojuarezrranz@gmail.com>
2018-07-10 19:31:05 +0200

    más

---
+++ mafiaCodeTest/src/main/java/com/stratio/MafiaCodeTestApplication.java
@@ -0,0 +1,12 @@
+package com.stratio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MafiaCodeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MafiaCodeTestApplication.class, args);
+	}
+}
